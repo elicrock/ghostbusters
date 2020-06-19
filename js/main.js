f0718a609fe5bf093fc082d8811e364a12b70dbf
Paul <paul.ivanov1990@gmail.com>
2020-06-20 00:10:36 +0300

    Homework

---
+++ js/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+  const mMenuBtn = $('.m-menu-button'),
+        mMenu = $('.m-menu'),
+        tab = $('.tab');
+
+  mMenuBtn.on('click', () => {
+    mMenu.toggleClass('active');
+    $('body').toggleClass('no-scroll');
+  });
+
+  tab.on('click', function() {
+    tab.removeClass('active')
+    $(this).toggleClass('active');
+    let activeTabContent =  $(this).attr('data-target');
+    $('.tabs-content').removeClass('visible');
+    $(activeTabContent).toggleClass('visible');
+  });
+
+  const mySwiper = new Swiper ('.swiper-container', {
+    slidesPerView: 4,
+    spaceBetween: 25,
+    loop: true,
+    breakpoints: {
+      992: {
+        slidesPerView: 4,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      320: {
+        slidesPerView: 1,
+        navigation: {
+          nextEl: '.button-next',
+        },
+      }
+    }
+  });
+});
